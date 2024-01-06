1c824e3b0ccdb6467c858a631f74320ea1fb45dd
tgaibotz <152408509+tgaibotz@users.noreply.github.com>
2024-01-06 05:21:03 +0500

    commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -200,7 +200,7 @@ async def start(client, message):
                 reply_markup=InlineKeyboardMarkup(
                     [
                      [
-                         InlineKeyboardButton('GENERAT FAST DOWNLOAD AND ONLINE WATCH LINK', callback_data=f'generate_stream_link:{file_id}')
+                         InlineKeyboardButton('💻 Watch online💻 • ⚡ Fast Download ⚡', callback_data=f'generate_stream_link:{file_id}')
                      ]
                     ]
                 )
@@ -373,15 +373,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
                 quote=True,
                 disable_web_page_preview=True,
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Fast Download 🚀", url=download),  # we download Link
-                                                    InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)]])  # web stream Link
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚡ Fast Download ⚡", url=download),  # we download Link
+                                                    InlineKeyboardButton('💻 Watch online 💻', url=stream)]])  # web stream Link
             )
             await query.message.reply_text(
                 text="•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ☠︎⚔",
                 quote=True,
                 disable_web_page_preview=True,
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Fast Download 🚀", url=download),  # we download Link
-                                                    InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)]])  # web stream Link
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚡ Fast Download ⚡", url=download),  # we download Link
+                                                    InlineKeyboardButton('💻 Watch online 💻', url=stream)]])  # web stream Link
             )
         except Exception as e:
             print(e)  # print the error message
