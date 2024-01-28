699c8f0304de819e6c097639be17b75519aa644a
AI REPOS 23 <93036340+ai-botz@users.noreply.github.com>
2024-01-28 11:38:54 +0500

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ CDB_NAME = environ.get("CDB_NAME", "haroonhassan1")
 DB_URI = environ.get("DB_URI", "mongodb+srv://haroonhassan1:haroonhassan1@haroonhassan1.hdbc1p4.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = environ.get("DB_NAME", "haroonhassan2")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6492431239:AAENl-kRCeN5kwn-6KnAHcQXQXBJDbLvDuU")
-BOT_USERNAME = environ.get("BOT_USERNAME", "Ai_file_store_bot") # your bot username without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "THE_AI_FILE_STORE_BOT") # your bot username without @
 PICS = (environ.get('PICS', 'https://telegra.ph/AI-11-01-4')).split() # Bot Start Picture
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "5")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "300")) # Time in Seconds
